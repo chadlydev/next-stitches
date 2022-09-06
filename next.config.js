a20338569e443999a0d5db2b5fe86d3beaea3618
Chadly Riedewald <no925.c@gmail.com>
2022-09-06 16:38:20 +0200

    build: added absolute imports functionality

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }]
+    });
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+        util: require.resolve('util/')
+      };
+    }
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
